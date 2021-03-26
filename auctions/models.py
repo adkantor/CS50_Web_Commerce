7d932af18325e32f56cc319844f650a0a9dfd3f3
Adam Kantor <kantor.a@usa.net>
2021-03-26 19:03:32 +0100

    models.py tidy-up

--- auctions/models.py
+++ auctions/models.py
@@ -17,8 +17,21 @@ class Category(models.Model):
 
 
 class Listing(models.Model):
-    """ Class to represent a listing. """
-    
+    """ Class to represent a listing. """    
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=9, decimal_places=2, default=0)
+    image_url = models.URLField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listings', null=True)
+    created_time = models.DateTimeField(auto_now_add=True, null=True)
+    last_modified = models.DateTimeField(auto_now=True, null=True)
+    is_active = models.BooleanField(default=True)
+    watchlisted_by = models.ManyToManyField(User, blank=True, related_name='watchlisted_items')
+
+    def __str__(self):
+        return f"{self.title} | {self.created_by} | {'Active' if self.is_active else 'Closed'} | {self.created_time}"
+
     def current_bid(self):
         """ Returns the highest bid for this listing (if has a valid bid). Returns None otherwise. """
         max_bid_price = self.bids.aggregate(max_bid=Max('price'))['max_bid']
@@ -36,14 +49,6 @@ class Listing(models.Model):
     @classmethod
     def category_counts(cls):
         """ Returns dictionary mapping {category id : (category name, count)}. """
-        
-        # d = dict()
-        # for cat in cls.CATEGORIES:
-        #     cat_id = cat[0]
-        #     cat_name = cat[1]
-        #     cat_count = cls.objects.filter(category=cat_id, is_active=True).count()
-        #     d[cat_id] = (cat_name, cat_count)
-        # return d
         d = dict()
         for cat in Category.objects.all():
             cat_id = cat.id
@@ -53,35 +58,23 @@ class Listing(models.Model):
         return d
 
 
-    # CATEGORIES = [
-    #     ("fashion", "Fashion"),
-    #     ("toys", "Toys"),
-    #     ("electronics", "Electronics"),
-    #     ("home", "Home"),
-    #     ("other", "Other")
-    # ]
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    starting_bid = models.DecimalField(max_digits=9, decimal_places=2, default=0)
-    image_url = models.URLField(blank=True, null=True)
-    # category = models.CharField(max_length=100, choices=CATEGORIES)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listings', null=True)
-    created_time = models.DateTimeField(auto_now_add=True, null=True)
-    last_modified = models.DateTimeField(auto_now=True, null=True)
-    is_active = models.BooleanField(default=True)
-    watchlisted_by = models.ManyToManyField(User, blank=True, related_name='watchlisted_items')
-
-
 class Bid(models.Model):
+    """ Class to represent a bid. """
     bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bids', null=True)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bids', null=True)
     time = models.DateTimeField(auto_now_add=True, null=True)
     price = models.DecimalField(max_digits=9, decimal_places=2, default=0)
 
+    def __str__(self):
+        return f"{self.bidder} | {self.listing.title} | ${self.price} | {self.time}"
+
 
 class Comment(models.Model):
+    """ Class to represent a comment. """
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments', null=True)
     created_time = models.DateTimeField(auto_now_add=True, null=True)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='comments', null=True)
     text = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.created_by} | {self.listing.title} | ${(self.text)[:30]}[...] | {self.created_time}"
